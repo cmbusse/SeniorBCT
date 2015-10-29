8f44c4e3e2f2c8d5cb66017edad9ee5d598f456c
Chris <cmbusse@comcast.net>
2015-10-29 13:37:27 -0400

    Small changes to dashboard

--- public/modules/users/controllers/users.client.controller.js
+++ public/modules/users/controllers/users.client.controller.js
@@ -3,17 +3,25 @@
 angular.module('users').controller('UsersController', ['$scope', '$rootScope', '$stateParams', '$location', 'Users', 'Authentication',
 	function($scope, $rootScope, $stateParams, $location, Users, Authentication) {
 		// Current User
-		$scope.user = Authentication.user;	
+		$scope.currentuser = Authentication.user;	
+		
 		// retrieve a list of users in the website
 		$scope.users = Users.query();
-		// Is logged in user an admin
-		$scope.loggedInIsAdmin = false;
-		if ($scope.user.roles[0] === 'admin'){
-			$scope.loggedInIsAdmin = true;
-		}
 		
+		// Is logged in user an admin
+		$scope.loggedInIsAdmin = function(){
+			return $scope.currentuser.roles[0] === 'admin';
+		};
+		
+		// Checks to see if a user is an admin
+		$scope.isAdmin = function(user){
+			return user.roles[0] === 'admin';
+		};
         
-		$scope.user2 = Authentication.user;	
+		// Checks to see if user is currently logged in
+		$scope.isCurrentUser = function(user){
+			return user._id === $scope.currentuser._id;
+		}
 
 	}
 ]);
