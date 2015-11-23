833cd3f8b668c9b0c0389fd0447ed0a9621162e6
Chris <chrismbusse@gmail.com>
2015-11-23 09:57:04 -0500

    Small changes and begun working on check in functionality

--- public/modules/users/controllers/checkin.client.controller.js
+++ public/modules/users/controllers/checkin.client.controller.js
@@ -1,52 +1,61 @@
 'use strict';
 
-angular.module('users').controller('CheckinController', ['$scope', '$http', '$location', 'Authentication',
-	function($scope, $http, $location, Authentication) {
+angular.module('users').controller('CheckinController', ['$scope', '$http', '$location', 'Authentication', 'Users',
+	function($scope, $http, $location, Authentication, Users) {
+		
 		$scope.authentication = Authentication;
+		$scope.currentuser = Authentication.user;
 		var signIn = false;
 		var signOut = true;
-		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
 
-				// And redirect to the children/create page
-				$location.path('/children/create');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};	
-
-        	 if(signOut === true)
-        	 	//This is temporary, leave it until there is a signin field.
-        	 	{$scope.signOutDisabled = true;}
-
-if(signOut === true){
-    $scope.disableSigninButton = function() {
-        	$scope.signOutDisabled = false;
-        	 $scope.signInDisabled = true;
-        	 signIn = true;
-        	 signOut = false;
-    };
-}
-    {
-    $scope.disableSignoutButton = function() {
-        	$scope.signInDisabled = false;
-        	//Re-enable the signin button is we are signed out, we will need this to check the "signed in" field for each child.
-        	 $scope.signOutDisabled = true;
-        	 signIn = false;
-        	 signOut = true;
-    };
-}
-		$scope.signin = function() {
-			$http.post('/auth/signin', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
-				// And redirect to the childview page for that user
-				$location.path('/childview');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
+		// Is logged in user an admin
+		$scope.loggedInIsAdmin = function(){
+			if($scope.currentuser){
+				return $scope.currentuser.roles === 'admin';	
+			}
+			else{
+				return false;
+			}
 		};
+
+		//Retrieve a specific user from the back end
+		/*
+		$scope.findOneUser = function() {
+       		var allUsers = Users.query({}, function(){
+	       		for(var i=0; i < allUsers.length; i++)
+	       		{
+	       			var currUser = allUsers[i];
+	       			if(currUser._id === $stateParams.userId)
+	       			{
+	       				$scope.user = currUser;
+	       			}
+	       		}
+        	});
+		};
+		*/
+
+		// "Sign in" a user
+		$scope.signinUser = function(isValid){
+			// take PIN, search users for match
+			// if no match say "unknown PIN" or something
+			// if match, direct to checkin/userId
+			// PIN is stord in 'credentials'
+
+			var userID = 'Not Found';
+
+			var allUsers = Users.query({}, function(){
+	       		for(var i=0; i < allUsers.length; i++)
+	       		{
+	       			var currUser = allUsers[i];
+	       			if(currUser.PIN === $scope.credentials.PIN)
+	       			{
+	       				userID = currUser._id;
+	       			}
+	       		}
+        	});
+			// Checks for not found
+
+		};
+
 	}
 ]);
