5054e06f6073fa7388991446beb3b7052b3be001
Chris <chrismbusse@gmail.com>
2015-11-30 00:15:20 -0500

    Work on Week Viewer, basic html and css set up, started work on week selection, going back in weeks is close but too tired to continue

--- public/modules/children/controllers/children.client.controller.js
+++ public/modules/children/controllers/children.client.controller.js
@@ -7,6 +7,14 @@ angular.module('children').controller('ChildrenController', ['$scope', '$statePa
 		$scope.currentuser = Authentication.user;
 		$scope.usersChildren = {};
 		$scope.newestChild = {};
+		$scope.weekOfDate = {};
+		$scope.monDate = {};
+		$scope.tueDate = {};
+		$scope.wedDate = {};
+		$scope.thuDate = {};
+		$scope.friDate = {};
+		$scope.satDate = {};
+		$scope.sunDate = {};
 
 		// Create new Child
 		$scope.create = function() {
@@ -135,9 +143,67 @@ angular.module('children').controller('ChildrenController', ['$scope', '$statePa
 			}
 		};
 
+		$scope.seedWeekView = function(){
+			var d = new Date();
+			var d2 = new Date();
+			var d3 = new Date();
+			var d4 = new Date();
+			var d5 = new Date();
+			var d6 = new Date();
+			var d7 = new Date();
+			var day = d.getDay();
+			var diff = d.getDate() - day + (day === 0 ? -6:1);
+			$scope.weekOfDate = new Date(d.setDate(diff));
+			$scope.monDate = new Date(d.setDate(diff));
+			$scope.tueDate = new Date(d2.setDate(diff+1));
+			$scope.wedDate = new Date(d3.setDate(diff+2));
+			$scope.thuDate = new Date(d4.setDate(diff+3));
+			$scope.friDate = new Date(d5.setDate(diff+4));
+			$scope.satDate = new Date(d6.setDate(diff+5));
+			$scope.sunDate = new Date(d7.setDate(diff+6));
+			console.log('test');
+		};
 
+		$scope.backOneWeek = function(){
+			// revisit, focus on how things are updating, maybe trying new dating them before setting, idk though.  Or try making new date with weekOfDate as value, might work
+			var d = new Date($scope.weekOfDate);
+			var d2 = new Date($scope.weekOfDate);
+			var d3 = new Date($scope.weekOfDate);
+			var d4 = new Date($scope.weekOfDate);
+			var d5 = new Date($scope.weekOfDate);
+			var d6 = new Date($scope.weekOfDate);
+			var d7 = new Date($scope.weekOfDate);
+			var diff = d.getDate();
+			diff = diff-7;
+			if(diff<=0){
+				$scope.weekOfDate = new Date(d.setDate(diff));
+				$scope.monDate = $scope.weekOfDate;
+				var day = d.getDay();
+				diff = d.getDate() - day + (day === 0 ? -6:1);
+				$scope.tueDate = new Date(d2.setDate(diff+1));
+				$scope.wedDate = new Date(d3.setDate(diff+2));
+				$scope.thuDate = new Date(d4.setDate(diff+3));
+				$scope.friDate = new Date(d5.setDate(diff+4));
+				$scope.satDate = new Date(d6.setDate(diff+5));
+				$scope.sunDate = new Date(d7.setDate(diff+6));
+			} else{
+				$scope.weekOfDate = new Date(d.setDate(diff));
+				$scope.monDate = $scope.weekOfDate;
+				$scope.tueDate = new Date(d2.setDate(diff+1));
+				$scope.wedDate = new Date(d3.setDate(diff+2));
+				$scope.thuDate = new Date(d4.setDate(diff+3));
+				$scope.friDate = new Date(d5.setDate(diff+4));
+				$scope.satDate = new Date(d6.setDate(diff+5));
+				$scope.sunDate = new Date(d7.setDate(diff+6));
+			}
+			console.log('test');
+
+		};
+
+		// NOTE:  setDate documentation:  http://www.w3schools.com/jsref/jsref_setdate.asp
+		// setDate can basically keep going in -7 intervals to keep moving back dates
 		// Copied from UI bootstrap example
-		$scope.mode = 'week';
+		/*
 		$scope.today = function() {
 			$scope.dt = new Date();
 		};
@@ -149,13 +215,10 @@ angular.module('children').controller('ChildrenController', ['$scope', '$statePa
 
 		// Disable weekend selection
 		$scope.disabled = function(date, mode) {
-			return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+			return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 
+				|| date.getDay() === 5 || date.getDay() === 4 || date.getDay() === 3 
+				|| date.getDay() === 2) );
 		};
-
-		$scope.toggleMin = function() {
-			$scope.minDate = $scope.minDate ? null : new Date();
-		};
-		$scope.toggleMin();
 
 		$scope.open = function($event) {
 			$event.preventDefault();
@@ -166,11 +229,13 @@ angular.module('children').controller('ChildrenController', ['$scope', '$statePa
 
 		$scope.dateOptions = {
 			formatYear: 'yy',
-			startingDay: 1
+			startingDay: 1,
+			showWeeks: false
 		};
 
 		$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
 		$scope.format = $scope.formats[0];
+		*/
 
 	}
 
