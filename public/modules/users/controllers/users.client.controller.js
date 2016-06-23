65a6a55fb83bb10fda1c995e6a1630b50161b47a
Chris <chrismbusse@gmail.com>
2016-06-23 17:06:35 -0400

    Fix for null child issue

--- public/modules/users/controllers/users.client.controller.js
+++ public/modules/users/controllers/users.client.controller.js
@@ -42,10 +42,12 @@ angular.module('users').controller('UsersController', ['$scope', '$http', '$root
 				for(var i=0; i < allChildren.length; i++)
 	       		{
 	       			var currChild = allChildren[i];
-	       			if(currChild.user._id === userid)
-	       			{
-	       				usersChildren.push(currChild);
-	       			}
+	       			if(currChild.user){
+		       			if(currChild.user._id === userid)
+		       			{
+		       				usersChildren.push(currChild);
+		       			}
+		       		}
 	       		}
 	       		$scope.usersChildren = usersChildren;
         	});
@@ -60,10 +62,12 @@ angular.module('users').controller('UsersController', ['$scope', '$http', '$root
 				for(var i=0; i < allChildren.length; i++)
 	       		{
 	       			var currChild = allChildren[i];
-	       			if(currChild.user._id === userid)
-	       			{
-	       				usersChildren.push(currChild);
-	       			}
+	       			if(currChild.user){
+		       			if(currChild.user._id === userid)
+		       			{
+		       				usersChildren.push(currChild);
+		       			}
+		       		}
 	       		}
 	       		$scope.usersChildren = usersChildren;
         	});
